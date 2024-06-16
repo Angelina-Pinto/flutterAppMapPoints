d3bf998fde417f39bad3f4aa12a62d5bc4589541
angelina_pg <angelinapinto127@gmail.com>
2024-06-15 22:23:50 -0600

    Ploteo de puntos

--- lib/src/views/screen_map.dart
+++ lib/src/views/screen_map.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:mapbox_polyline_points/mapbox_polyline_points.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_points/src/models/position.dart';
 
 const MAPBOX_ACCESS_TOKEN =
     'pk.eyJ1IjoiYXBpbnRvNyIsImEiOiJjbHhkdnQ2ZnYwYXhqMnFvdmJlZG9va3Y3In0.0PZU4RBLEl4W16Fxqksf0g';
-//const MAPBOX_STYLE = 'mapbox/streets-v11';
 const MAPBOX_STYLE = 'mapbox/outdoors-v12';
 
-final pos = LatLng(14.521457, -90.5668893);
+final pos = LatLng(14.4887947, -90.5809932);
 
-//final List<String> abilities = [’listado’]
+final objpos1 = Position(lat: 14.52516, lon:  -90.56036);
+final objpos2 = Position(lat: 14.52477, lon: -90.56107);
+final objpos3 = Position(lat: 14.5245, lon: -90.56142);
+final objpos4 = Position(lat: 14.52429, lon:  -90.5617);
+final objpos5 = Position(lat: 14.52411, lon:  -90.56193);
+final objpos6 = Position(lat: 14.5237, lon: -90.56249);
+final objpos7 = Position(lat: 14.5235, lon: -90.56271);
+final objpos8 = Position(lat: 14.52328, lon: -90.56307);
+final objpos9 = Position(lat: 14.52291, lon: 90.56334);
+final objpos10 = Position(lat: 14.5224, lon: -90.56354);
+final objpos11 = Position(lat: 14.52182, lon:  -90.56368);
+final objpos12 = Position(lat: 14.52108, lon:  -90.56378);
+final objpos13 = Position(lat: 14.51984, lon: -90.56388);
+final objpos14 = Position(lat: 14.51867, lon: -90.56411);
+final objpos15 = Position(lat: 14.5156, lon: -90.56391);
 
-final objpos1 = new Position(lat: 14.521457, lon: -90.5668893);
-final objpos2 = new Position(lat: 14.5215417, lon: -90.6055141);
-final objpos3 = new Position(lat: 14.614528, lon: -90.5183232);
-final objpos4 = new Position(lat: 14.614528, lon: -90.5183232);
 
-
-final List<Position> list_pos = [objpos1, objpos2, objpos3, objpos4];
+final List<Position> list_pos = [objpos1, objpos2, objpos3, objpos4, objpos5, objpos6, objpos7, objpos8, objpos9, objpos9, objpos10
+,objpos11, objpos12, objpos13, objpos14, objpos15];
 
 class FlutterScreenMap extends StatelessWidget {
   const FlutterScreenMap({super.key});
@@ -36,7 +44,11 @@ class FlutterScreenMap extends StatelessWidget {
           Flexible(
             flex: 5,
             child: FlutterMap(
-              options: MapOptions(center: pos,zoom: 10,minZoom: 5,maxZoom: 25,
+              options: MapOptions(
+                center: pos,
+                zoom: 10,
+                minZoom: 5,
+                maxZoom: 25,
               ),
               children: [
                 TileLayer(
@@ -48,17 +60,12 @@ class FlutterScreenMap extends StatelessWidget {
                   },
                   userAgentPackageName: 'com.example.app',
                 ),
-                
-              ],
-              
-              nonRotatedChildren: [
-            
                 MarkerLayer(
-                  markers: [
-                    Marker(
+                  markers: list_pos.map((position) {
+                    return Marker(
                       width: 80.0,
                       height: 80.0,
-                      point: pos,
+                      point: LatLng(position.lat, position.lon),
                       builder: (ctx) => Container(
                         child: Icon(
                           Icons.location_on,
@@ -66,32 +73,33 @@ class FlutterScreenMap extends StatelessWidget {
                           size: 40.0,
                         ),
                       ),
-                    ),
-                  ],
+                    );
+                  }).toList(),
                 ),
               ],
-              
             ),
-           
           ),
           Flexible(
-              child: ListView.builder(
-                  itemCount: list_pos.length,
-                  itemBuilder: (context, index) {
-                    final position = list_pos[index];
-                  return ListTile(
+            child: ListView.builder(
+              itemCount: list_pos.length,
+              itemBuilder: (context, index) {
+                final position = list_pos[index];
+                return ListTile(
                   title: Text(
                     'Posición ${index + 1}: (${position.lat}, ${position.lon})',
                   ),
                 );
-                  }
-            )
-          )
+              },
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-
-
+void main() {
+  runApp(MaterialApp(
+    home: FlutterScreenMap(),
+  ));
+}
